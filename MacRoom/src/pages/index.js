62bcc440337b9483e0946ce1acb5f1741d688a21
Axel De Roeck <axel.deroeck@student.ap.be>
2020-12-02 14:11:29 +0100

    hoofdletter naar kleine letter Layout & Seo

--- MacRoom/src/pages/index.js
+++ MacRoom/src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { Container, Image, Product } from "../pageStyles/pageStyles"
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const IndexPage = () => {
   const {
